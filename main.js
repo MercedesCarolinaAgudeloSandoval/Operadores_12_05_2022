32e19fed0633da7d0497e703cfe53ed94e56a150
MercedesCarolinaAgudeloSandoval <mercedes.agudelo@gmail.com>
2022-05-17 18:31:16 -0500

    Operadores modificacion

--- main.js
+++ main.js
@@ -113,3 +113,123 @@ document.querySelector("#multi").insertAdjacentHTML("beforeend", `
         <mark>c</mark> : <em>${c}</em>
     </div>
 `);
+
+/////////////////Division/////////////
+
+let e = 10;
+let f = 3;
+let resD = e / f;
+
+let selD = document.querySelectorAll("#divi em");
+selD[0].insertAdjacentText("beforeend", e);
+selD[1].insertAdjacentText("beforeend", f);
+selD[2].insertAdjacentText("beforeend", resD);
+
+// Significado
+resD = e / f;
+document.querySelector("#divi").insertAdjacentHTML("beforeend", `
+    <div>
+        <b>Significado División</b>
+    </div>
+    <hr>
+    <div>
+        <em>resD = e / f;</em>
+        <br>
+        <mark>resD</mark> : <em>${resD}</em>
+    </div>
+`);
+
+// Abreviado
+e /= f;
+document.querySelector("#divi").insertAdjacentHTML("beforeend", `
+    <br>
+    <div>
+        <b>Abreviado División</b>
+    </div>
+    <hr>
+    <div>
+        <em>e /= f;</em>
+        <br>
+        <mark>e</mark> : <em>${e}</em>
+    </div>
+`);
+
+
+/////////////////Asignación Resto/////////////
+
+let g = 10;
+let h = 3;
+let resAR = g % h;
+
+let selAR= document.querySelectorAll("#restoA em");
+selAR[0].insertAdjacentText("beforeend", g);
+selAR[1].insertAdjacentText("beforeend", h);
+selAR[2].insertAdjacentText("beforeend", resAR);
+
+// Significado
+resAR = g % h;
+document.querySelector("#restoA").insertAdjacentHTML("beforeend", `
+    <div>
+        <b>Significado Asignación Resto</b>
+    </div>
+    <hr>
+    <div>
+        <em>resD = g % h;</em>
+        <br>
+        <mark>resAR</mark> : <em>${resAR}</em>
+    </div>
+`);
+
+// Abreviado
+g %= h;
+document.querySelector("#restoA").insertAdjacentHTML("beforeend", `
+    <br>
+    <div>
+        <b>Abreviado Asignación Resto</b>
+    </div>
+    <hr>
+    <div>
+        <em>g %= h;</em>
+        <br>
+        <mark>g</mark> : <em>${g}</em>
+    </div>
+`);
+/////////////////Asignación Operador Exponencial/////////////
+
+let i = 10;
+let j = 3;
+let resExp = i % j;
+
+let selExp= document.querySelectorAll("#restoExp em");
+selExp[0].insertAdjacentText("beforeend", i);
+selExp[1].insertAdjacentText("beforeend", j);
+selExp[2].insertAdjacentText("beforeend", resExp);
+
+// Significado
+resExp = i ** j;
+document.querySelector("#restoA").insertAdjacentHTML("beforeend", `
+    <div>
+        <b>Significado Operador Exponencial</b>
+    </div>
+    <hr>
+    <div>
+        <em>resD = i ** j;</em>
+        <br>
+        <mark>resExp</mark> : <em>${resExp}</em>
+    </div>
+`);
+
+// Abreviado
+i **= j;
+document.querySelector("#restoExp").insertAdjacentHTML("beforeend", `
+    <br>
+    <div>
+        <b>Abreviado Operador Exponencial</b>
+    </div>
+    <hr>
+    <div>
+        <em>i **= j;</em>
+        <br>
+        <mark>i</mark> : <em>${i}</em>
+    </div>
+`);
