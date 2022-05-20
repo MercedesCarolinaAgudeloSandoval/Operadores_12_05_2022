5b33a7f022bd9fbe61c9b40e7932005a9b390af1
MercedesCarolinaAgudeloSandoval <mercedes.agudelo@gmail.com>
2022-05-19 21:29:07 -0500

    Modificacion XOR

--- Operador_XOR/main.js
+++ Operador_XOR/main.js
@@ -1,10 +1,8 @@
 let A = Number(prompt("Ingrese el valor de la variable A\n 0 = false\n 1 = true"));
-let B = Number(prompt("Ingrese el valor de la variable B\n 0 = false\n 1 = true"));
 
 document.querySelector("#operadorXOR").insertAdjacentHTML("beforeend", `
     <tr class="resultado">
         <td>${A}</td>
-        <td>${B}</td>
-        <td>${A (!) B}</td>
+        <td>${!A}</td>
     </tr>
 `);
